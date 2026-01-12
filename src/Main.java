283da821cb0fbf63c52e958c3a60a97b37c4eb65
john <john.silvaromao@gmail.com>
2026-01-12 11:46:33 -0300

    Versao. Estudos de POO. sobrecarregueio o construtor, adicionei uma condicional e adicionei getters e setters.

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,46 @@
-void main() {
-    Scanner sc = new Scanner(System.in);
+import java.util.Scanner;
 
-    System.out.println("Insira os dados do produto");
-    System.out.print("nome: ");
-    String nome = sc.nextLine();
-    System.out.print("preço: ");
-    double preco = sc.nextDouble();
-    System.out.println("já Há quantidade no estoque?");
-    boolean s = sc.nextBoolean();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    if (s){
-        System.out.print("Quantidade: ");
-        int quantidade = sc.nextInt();
-        Produto produto = new Produto(nome,preco,quantidade);
-        System.out.println("dados do produto: "+ produto);
+        System.out.println("Insira os dados do produto");
+        System.out.print("nome: ");
+        String nome = sc.nextLine();
+        System.out.print("preço: ");
+        double preco = sc.nextDouble();
+        System.out.println("já Há quantidade no estoque?");
+        boolean s = sc.nextBoolean();
+
+        if (s) {
+            System.out.print("Quantidade: ");
+            int quantidade = sc.nextInt();
+            Produto produto = new Produto(nome, preco, quantidade);
+            System.out.println("dados do produto: " + produto);
+            System.out.print("adicione mais quanidade do produto: ");
+            int add = sc.nextInt();
+
+            produto.adicionar(add);
+            System.out.println("dados do produto: " + produto);
+            System.out.print("remova quanidade do produto: ");
+            int rem = sc.nextInt();
+
+            produto.remover(rem);
+            System.out.println("dados do produto: " + produto);
+            System.exit(0);
+        }
+        Produto produto = new Produto(nome, preco);
+        System.out.println("dados do produto: " + produto);
         System.out.print("adicione mais quanidade do produto: ");
         int add = sc.nextInt();
 
         produto.adicionar(add);
-        System.out.println("dados do produto: "+produto);
+        System.out.println("dados do produto: " + produto);
         System.out.print("remova quanidade do produto: ");
         int rem = sc.nextInt();
 
         produto.remover(rem);
-        System.out.println("dados do produto: "+produto);
-        System.exit(0);
+        System.out.println("dados do produto: " + produto);
+
     }
-        Produto produto = new Produto(nome, preco);
-    System.out.println("dados do produto: "+ produto);
-    System.out.print("adicione mais quanidade do produto: ");
-    int add = sc.nextInt();
-
-    produto.adicionar(add);
-    System.out.println("dados do produto: "+produto);
-    System.out.print("remova quanidade do produto: ");
-    int rem = sc.nextInt();
-
-    produto.remover(rem);
-    System.out.println("dados do produto: "+produto);
-
 }
